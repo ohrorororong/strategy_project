14d5704a7ef25be1cc7f3464d27ad0213ba8b341
suzhanlee <suzhanlee>
2022-07-20 16:47:46 +0900

    feat: Portal레파지토리 입니다.

---
+++ src/character/portal/Portal.java
@@ -0,0 +1,28 @@
+package character.portal;
+
+public abstract class Portal {
+
+    private String name;
+    private String intorduce;
+
+    public Portal(String name, String intorduce) {
+        this.name = name;
+        this.intorduce = intorduce;
+    }
+
+    public abstract void move();
+
+
+    public void changePortal(){
+
+    }
+
+
+    @Override
+    public String toString() {
+        return "Portal{" +
+                "name='" + name + '\'' +
+                ", intorduce='" + intorduce + '\'' +
+                '}';
+    }
+}
---
+++ src/character/portal/repository/PortalRepository.java
@@ -0,0 +1,15 @@
+package character.portal.repository;
+
+import character.portal.Portal;
+
+import java.util.List;
+
+public interface PortalRepository {
+
+    List<Portal> findAllMap();
+
+
+
+
+
+}
