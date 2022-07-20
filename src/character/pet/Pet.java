f5bdafc32a914e402a6cc1ccd0f46341409abb11
leetmdgus <lee_tmdgus@naver.com>
2022-07-20 16:48:07 +0900

    add repository

--- src/character/pet/Pet.java
+++ src/character/pet/Pet.java
@@ -1,4 +1,21 @@
 package character.pet;
 
-public class Pet {
+abstract public class Pet {
+    private String name;
+    private String introduce;
+
+    public Pet(String name, String introduce) {
+        this.name = name;
+        this.introduce = introduce;
+    }
+
+    abstract public void sound();
+
+    @Override
+    public String toString() {
+        return "Pet{" +
+                "name='" + name + '\'' +
+                ", introduce='" + introduce + '\'' +
+                '}';
+    }
 }
