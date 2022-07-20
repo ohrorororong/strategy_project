9216962cc5346e9282d52fb6732f71848fac613d
ohrorororong <hasfw@naver.com>
2022-07-20 16:48:15 +0900

    feat: 레포지토리 생성 및 추상클래스 추가

--- src/character/weapon/Weapon.java
+++ src/character/weapon/Weapon.java
@@ -33,4 +33,6 @@ public abstract class Weapon {
                 ", introduce='" + introduce + '\'' +
                 '}';
     }
+
+
 }
--- src/character/weapon/repository/WeaponRepository.java
+++ src/character/weapon/repository/WeaponRepository.java
@@ -5,6 +5,7 @@ import character.weapon.Weapon;
 import java.util.List;
 
 public interface WeaponRepository {
+
     List<Weapon> findAllWeapon();
     Weapon findByWeaponId(Long id);
 }
