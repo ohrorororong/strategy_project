2ea25109fa3bfb83eb0c21da934f00a85029b455
suzhanlee <suzhanlee>
2022-07-20 16:47:46 +0900

    feat: Portal레파지토리 입니다.

---
+++ src/character/portal/repository/Impl/PortalRepositoryImpl.java
@@ -0,0 +1,36 @@
+package character.portal.repository.Impl;
+
+import character.portal.Portal;
+import character.portal.product.Hospital;
+import character.portal.product.HuntingGround;
+import character.portal.repository.PortalRepository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class PortalRepositoryImpl implements PortalRepository {
+
+    private static Map<Long, Portal> store = new HashMap<>();
+
+    public PortalRepositoryImpl(){
+        portalInit();
+
+
+    }
+
+    public void portalInit(){
+
+        store.put(1L , new Hospital("병원", "병원입니다."));
+        store.put(2L , new HuntingGround("사냥터", "사냥터입니다."));
+    }
+
+
+
+
+    @Override
+    public List<Portal> findAllMap() {
+        return new ArrayList<>(store.values());
+    }
+}
